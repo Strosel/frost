850f7b52f723061869fa296a140813a1140af8ce
axel paulander <axel.paulander@gmail.com>
2022-07-02 00:35:56 +0200

    Const generic text widgets

--- src/pure/text.rs
+++ src/pure/text.rs
@@ -1,18 +1,32 @@
-use {iced_native::text::Renderer, iced_pure::widget::Text};
-
 macro_rules! generate_text_fn {
     ($($fn:ident = $size:literal),+) => {
         $(
-        #[doc = concat!("Create a header of size ", stringify!($size))]
-        pub fn $fn<T, R>(text: T) -> Text<R>
-        where
-            T: Into<String>,
-            R: Renderer,
-        {
-            Text::new(text).size($size)
-        }
+        #[doc = concat!("A header of size ", stringify!($size))]
+        pub type $fn = Text<$size>;
         )*
     };
 }
 
 generate_text_fn![H0 = 40, H1 = 32, H2 = 26, H3 = 22, H4 = 20];
+
+///Intermediate widget for `iced_pure::widget::Text` with size `SIZE`
+///
+///used mainly to define shorthands for text of specific sizes
+///see [`H0`](../H0), [`H1`](../H1), [`H2`](../H2), [`H3`](../H3) and [`H4`](../H4)
+pub struct Text<const SIZE: u16>;
+
+#[allow(clippy::new_ret_no_self)]
+impl<const SIZE: u16> Text<SIZE> {
+    pub fn new<T, R>(text: T) -> iced_pure::widget::Text<R>
+    where
+        T: Into<String>,
+        R: iced_native::text::Renderer,
+    {
+        iced_pure::widget::Text::new(text).size(SIZE)
+    }
+
+    ///Fetch the `SIZE` for uses elsewhere
+    pub const fn size() -> u16 {
+        SIZE
+    }
+}
