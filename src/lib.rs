fd22188e4fd6d54c6d906cdc463b758d5109513e
axel paulander <axel.paulander@gmail.com>
2023-02-14 00:58:13 +0100

    Add wrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+//TODO kitchen sink example
 #[allow(non_snake_case)]
 ///button with container properties
 pub mod clickable;
@@ -5,3 +6,6 @@ pub mod clickable;
 #[allow(non_snake_case)]
 ///text widgets with default sizes
 pub mod text;
+
+///Wrapping containers
+pub mod wrap;
---
+++ src/wrap.rs
@@ -0,0 +1,120 @@
+use iced_native::{
+    widget::{Column, Row},
+    Alignment, Element, Length,
+};
+
+///The main direction of [`Wrap`]
+///
+///Funtions similar to `flex-direction` in CSS flexbox. A wrap will grow in the specified
+///and wrap every n elements as specified by the contained value
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Row(u32),
+    Column(u32),
+}
+
+///A container that distributes its contents in both directions by wrapping
+pub struct Wrap<'a, Message, Renderer> {
+    direction: Direction,
+    width: Length,
+    height: Length,
+    spacing: u16,
+    padding: u16,
+    children: Vec<Element<'a, Message, Renderer>>,
+}
+
+impl<'a, Message, Renderer> Wrap<'a, Message, Renderer> {
+    ///Creates an empty [`Wrap`]
+    pub fn new(direction: Direction) -> Self {
+        Self::with_children(direction, Vec::new())
+    }
+
+    ///Creates a [`Wrap`] with the given elements
+    pub fn with_children(
+        direction: Direction,
+        children: Vec<Element<'a, Message, Renderer>>,
+    ) -> Self {
+        Self {
+            direction,
+            width: Length::Shrink,
+            height: Length::Shrink,
+            spacing: 0,
+            padding: 0,
+            children,
+        }
+    }
+
+    ///Sets the width of the [`Wrap`]
+    pub fn width(mut self, width: Length) -> Self {
+        self.width = width;
+        self
+    }
+
+    ///Sets the height of the [`Wrap`]
+    pub fn height(mut self, height: Length) -> Self {
+        self.height = height;
+        self
+    }
+
+    ///Sets the spacing *between* elements
+    pub fn spacing(mut self, units: u16) -> Self {
+        self.spacing = units;
+        self
+    }
+
+    ///Sets the padding of the [`Wrap`]
+    pub fn padding(mut self, units: u16) -> Self {
+        self.padding = units;
+        self
+    }
+
+    ///Adds an [`Element`] to the [`Wrap`]
+    pub fn push<E>(mut self, child: E) -> Self
+    where
+        E: Into<Element<'a, Message, Renderer>>,
+    {
+        self.children.push(child.into());
+        self
+    }
+}
+
+impl<'a, Message, Renderer> From<Wrap<'a, Message, Renderer>> for Element<'a, Message, Renderer>
+where
+    Message: 'a,
+    Renderer: iced_native::Renderer + 'a,
+{
+    fn from(wrap: Wrap<'a, Message, Renderer>) -> Self {
+        //defining the macro here allows it to access wrap without passing it
+        macro_rules! resolve {
+            //Double brackets to allow a more complex macro
+            //blocks are considered a single expression
+            ($limit:ident, $main:ident, $cross:ident) => {{
+                let mut cross = $cross::new()
+                    .align_items(Alignment::Center)
+                    .width(wrap.width)
+                    .height(wrap.height)
+                    .padding(wrap.padding)
+                    .spacing(wrap.spacing);
+                let mut iter = wrap.children.into_iter().peekable();
+
+                while iter.peek().is_some() {
+                    let mut main = $main::new().spacing(wrap.spacing);
+
+                    for _ in 0..$limit {
+                        let Some(child) = iter.next() else { break };
+                        main = main.push(child);
+                    }
+
+                    cross = cross.push(main);
+                }
+
+                cross.into()
+            }};
+        }
+
+        match wrap.direction {
+            Direction::Row(limit) => resolve!(limit, Row, Column),
+            Direction::Column(limit) => resolve!(limit, Column, Row),
+        }
+    }
+}
